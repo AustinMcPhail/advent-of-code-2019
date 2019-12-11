78155ba1d683f9f39002484b542f9a8ac0df2749
Austin McPhail <austin@mcphail.dev>
2019-12-10 23:10:45 -0600

    feat(10): december 10th - part 1 complete

--- 10/ten.go
+++ 10/ten.go
@@ -11,48 +11,73 @@ import (
 const ASTEROID = "#"
 const NOTHING = "."
 
+type Asteroid struct {
+	distanceTo float64
+	x          int
+	y          int
+}
+
 func main() {
 	m, height, width := readFile("input")
 	posX := 0
 	posY := 0
 	count := 0
+	var asteroids map[float64][]Asteroid
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			if m[y][x] == ASTEROID {
-				c := findAsteroidsInSight(x, y, m, height, width)
+				c, a := findAsteroidsInSight(x, y, m, height, width)
 				if c >= count {
 					count = c
 					posX = x
 					posY = y
+					asteroids = a
 				}
 			}
 		}
 	}
 	fmt.Printf("%d Asteroids seen from position [%d, %d]", count, posX, posY)
+	fmt.Printf("Commencing asteroid evaporation from position [%d, %d]", posX, posY)
 }
 
-func findAsteroidsInSight(x int, y int, m map[int]map[int]string, height int, width int) int {
-	angles := map[float64]bool{}
+func findAsteroidsInSight(x int, y int, m map[int]map[int]string, height int, width int) (int, map[float64][]Asteroid) {
+	angles := map[float64][]Asteroid{}
 	count := 0
 	for y2 := 0; y2 < height; y2++ {
 		for x2 := 0; x2 < width; x2++ {
-			fmt.Printf("Checking pos1[%d, %d] and pos2[%d, %d]\n", x, y, x2, y2)
-			if m[y2][x2] == ASTEROID && x != x2 && y != y2 {
-				fmt.Printf("Found Asteroid at pos2[%d, %d]\n", x2, y2)
-				if _, ok := angles[getAngle(x, y, x2, y2)]; !ok {
+			if m[y2][x2] == ASTEROID && !(x == x2 && y == y2) {
+				a := getAngle(x, y, x2, y2)
+				if _, ok := angles[a]; !ok {
 					count++
+					angles[a] = []Asteroid{}
+					angles[a] = append(angles[a], Asteroid{
+						distanceTo: getDistance(x, y, x2, y2),
+						x:          x2,
+						y:          y2,
+					})
+				} else {
+					angles[a] = append(angles[a], Asteroid{
+						distanceTo: getDistance(x, y, x2, y2),
+						x:          x2,
+						y:          y2,
+					})
 				}
 			}
 		}
 	}
-	return count
+	return count, angles
+}
+
+func getDistance(x1 int, y1 int, x2 int, y2 int) float64 {
+	x := math.Pow(float64(x2 - x1), 2)
+	y := math.Pow(float64(y2 - y1), 2)
+	return math.Sqrt(x + y)
 }
 
 func getAngle(x1 int, y1 int, x2 int, y2 int) float64 {
 	dX := x2 - x1
 	dY := y1 - y2
 	r := math.Atan2(float64(dY), float64(dX))
-	fmt.Println(r)
 	return r
 }
 
